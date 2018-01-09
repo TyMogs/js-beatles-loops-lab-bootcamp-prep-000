47ecbfd804ff13dd7363f9ce9753d5d74447b39e
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 23:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function johnLennonFacts(facts) {
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"];
+  var factsNew = []
   var i = 0;
   while (i < facts.length) {
     facts[i].push("!!!")
     i++
   }
-  return facts
+  return factsNew
 }
