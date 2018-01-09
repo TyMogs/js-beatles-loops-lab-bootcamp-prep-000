d593212901cda76f2e83c42894e7b8fc5909feda
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 23:19:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function theBeatlesPlay(mArray, iArray) {
   var array = []
   
-  var idx = 0
-  for (idx = 0; idx < mArray; idx++) {
+  for (var idx = 0; idx < mArray; idx++) {
     array.push(`${mArray[idx]} plays ${iArray[idx]}`)
   }
   return array
