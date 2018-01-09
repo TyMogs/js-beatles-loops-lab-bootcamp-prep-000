22a1e166efca168ece5f817c4a4daf1c5aef4150
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 23:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  var result = [];
   var factsNew = []
   var i = 0;
   while (i < facts.length) {
-    factsNew.push(`${facts[i]}!!!`)
+    result.push(`${facts[i]}!!!`)
     i++
   }
   return factsNew
